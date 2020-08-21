37d96c19104a341d30c1f4e63c006c069a863a0c
Andrea Caciolai <andrea.caciolai@gmail.com>
2020-08-21 19:06:04 +0200

    Switched to Three.js

--- main.js
+++ main.js
@@ -1,55 +1,40 @@
-// Document variables
-var canvas;
-var engine;
-
-// Scene variables
 var scene;
-var camera
+var camera;
+var renderer;
+
+var cube;
 
 function init() {
-    canvas = document.getElementById('renderCanvas');
-    engine = new BABYLON.Engine(canvas, true);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 }
 
 function createScene() {
-    // Create a basic BJS Scene object.
-    scene = new BABYLON.Scene(engine);
 
-    // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-    camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 5,-10), scene);
-
-    // Target the camera to scene origin.
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // Attach the camera to the canvas.
-    camera.attachControl(canvas, false);
-
-    // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-
-    // Create a built-in "sphere" shape. 
-    var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {segments:16, diameter:2}, scene);
-
-    // Move the sphere upward 1/2 of its height.
-    sphere.position.y = 1;
-
-    // Create a built-in "ground" shape.
-    var ground = BABYLON.MeshBuilder.CreateGround('ground1', {height:6, width:6, subdivisions: 2}, scene);
-
-    // Return the created scene.
-    return scene;
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    camera.position.z = 5;
 }
 
-var main = function() {
-    init();
+function animate() {
+    requestAnimationFrame( animate );
 
-    scene = createScene();
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-    engine.runRenderLoop(function() {
-        scene.render();
-    });
-
-    window.addEventListener('resize', function() {
-        engine.resize();
-    });
+    renderer.render( scene, camera );
 };
+
+function main() {
+    init();
+    createScene();
+    animate();
+}
+main();
